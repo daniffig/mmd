6dd21811ccff9c5f2d5166ad80a2a1bdd286ddf3
daniffig <daniffig@gmail.com>
2012-06-10 16:31:52 -0300

    Reinicio del repositorio.

--- apps/ventas/modules/marca/actions/actions.class.php
+++ apps/ventas/modules/marca/actions/actions.class.php
@@ -3,7 +3,8 @@
 require_once dirname(__FILE__).'/../lib/marcaGeneratorConfiguration.class.php';
 require_once dirname(__FILE__).'/../lib/marcaGeneratorHelper.class.php';
 
-/**
+/** (!$request->getParameter('marca[es_activo]')) && $this->Marca->getEsActivo() && $this->Marca->puedenBorrarme()) || !$this->Marca->getEsActivo() || $request->getParameter('marca[es_activo]')
+ *
  * marca actions.
  *
  * @package    mmd
@@ -33,4 +34,24 @@ class marcaActions extends autoMarcaActions
 
     $this->redirect('@marca');
   }
+  
+  public function executeUpdate(sfWebRequest $request)
+  {
+
+    $this->Marca = $this->getRoute()->getObject();
+    $this->form = $this->configuration->getForm($this->Marca);
+    $arregloParametros = $request->getParameter('marca');
+	
+    if((!isset($arregloParametros['es_activo']) && $this->Marca->getEsActivo() && $this->Marca->puedenBorrarme()) || !$this->Marca->getEsActivo() || isset($arregloParametros['es_activo']))
+    {
+      $this->processForm($request, $this->form);
+    }
+    else
+    {
+      $this->getUser()->setFlash('error', 'La Marca tiene Productos asociados.');
+      $this->redirect(array('sf_route' => 'marca_edit', 'sf_subject' => $this->Marca));
+    }
+    $this->setTemplate('edit');
+  }
+  
 }
